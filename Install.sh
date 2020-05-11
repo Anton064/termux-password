cf4fd98b1456bae10795eead6a7be103dd38e330
Anton064 <63185453+Anton064@users.noreply.github.com>
2020-05-11 09:57:46 +0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,5 @@
+apt update && apt upgrade
+cd && cd .. && cd usr/etc && rm -rf bash.bashrc && cd && clear
+cd && cp -r termux-password /data/data/com.termux/files/usr/etc && cd && clear
+cd && cd .. && cd usr/etc/termux-password
+cp -r bash.bashrc /data/data/com.termux/files/usr/etc && cd && clear
